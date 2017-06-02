c724895de8702f9ef218d83666fbc3e46d8d4baf
王斯晨 <wangsichen@qudian.com>
2017-06-02 11:00:07 +0800

    Initial Commit

---
+++ UIView-CAAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIView-CAAnimation
+//
+//  Created by Jason on 2017/6/2.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
